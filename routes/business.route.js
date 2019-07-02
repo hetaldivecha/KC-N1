ad7c8a8eb3b1f630bca24729bea1fd2a8586720f
hetaldivecha <divechahetal@gmail.com>
2019-07-02 21:56:26 +0530

    upload change directory

--- routes/business.route.js
+++ routes/business.route.js
@@ -12,7 +12,7 @@ var store = multer.diskStorage({
      //local
     //cb(null, '../KC-Backend/img_uploads');
     //Build
-    cb(null, '../public/upload/');
+    cb(null, '../public/img_uploads/');
   },
   filename:function(req,file,cb){
       cb(null, Date.now()+'.'+file.originalname);
